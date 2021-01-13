f57fccae5d5ef6736ca1496749a3419701aa3748
Mike MacCana <mike.maccana@gmail.com>
2021-01-13 14:53:27 +0000

    Style UI for app instead of document

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -21,6 +21,12 @@
 		margin: 0 auto;
 		display: grid;
 		grid-template-columns: var(--single-column-text-max) var(--labeller-width);
+		display: grid;
+		grid-gap: 12px;
+		width: 100vw;
+		justify-content: center;
+		/* Stops the download bar (desktop) and the auto-hiding address bar (mobile) affecting the layout */
+		height: -webkit-fill-available;
 	}
 
 	h1 {
